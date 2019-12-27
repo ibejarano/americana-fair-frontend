ce69b529132546e4fd8ae6dfb4091bc865558489
Ignacio Bejarano <ignbejarano@gmail.com>
2019-12-27 20:17:32 -0300

    implemented new signin page

---
+++ pages/signin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignUpForm from '../components/SignUp';
+import LogIn from '../components/Login';
+
+const SignInPage = () => {
+        return (
+         <div>
+                 <SignUpForm />
+                 <LogIn />
+          </div>
+    )
+}
+
+export default SignInPage;
